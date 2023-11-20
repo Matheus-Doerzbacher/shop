8d3d97916adef5708ed987fb9b8597b03d53e6b0
MathePLay <Matheus.doer@gmail.com>
2023-11-20 20:58:53 -0300

    adicionada a funcao de deletar itens do carrinho

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -11,29 +13,53 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
       ),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: Theme.of(context).primaryColor,
-          child: Padding(
-            padding: const EdgeInsets.all(5),
-            child: FittedBox(
-              child: Text(
-                "${cartItem.price}",
-                style: const TextStyle(color: Colors.white),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text(
+                  "${cartItem.price}",
+                  style: const TextStyle(color: Colors.white),
+                ),
               ),
             ),
           ),
-        ),
-        title: Text(cartItem.name),
-        subtitle: Text("Total: R\$ ${cartItem.price * cartItem.quantity}"),
-        trailing: Text(
-          "${cartItem.quantity}x",
-          style: const TextStyle(fontSize: 14),
+          title: Text(cartItem.name),
+          subtitle: Text("Total: R\$ ${cartItem.price * cartItem.quantity}"),
+          trailing: Text(
+            "${cartItem.quantity}x",
+            style: const TextStyle(fontSize: 14),
+          ),
         ),
       ),
     );
