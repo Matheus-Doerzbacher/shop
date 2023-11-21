dd30d88d10733eb69af7ab03ba1c650ad77e548e
MathePLay <Matheus.doer@gmail.com>
2023-11-21 09:12:17 -0300

    finalizado

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/order_item.dart';
+import 'package:shop/models/order_list.dart';
 
 class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final OrderList orders = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -16,6 +21,15 @@ class OrdersPage extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: orders.itemsCount,
+          itemBuilder: (ctx, i) {
+            return OrderItem(order: orders.items[i]);
+          },
+        ),
+      ),
     );
   }
 }
