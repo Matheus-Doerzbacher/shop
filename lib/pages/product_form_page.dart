76d38329a64050bbb0ff2df86e31988927be703f
MathePLay <Matheus.doer@gmail.com>
2023-11-22 10:50:04 -0300

    finalizada as validações do Formulario

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -12,6 +15,9 @@ class _ProductFormPageState extends State<ProductFormPage> {
   final _descriptionFocus = FocusNode();
   final _imageUrlFocus = FocusNode();
   final _imageUrlController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+  final _formData = <String, Object>{};
 
   @override
   void initState() {
@@ -32,6 +38,30 @@ class _ProductFormPageState extends State<ProductFormPage> {
     setState(() {});
   }
 
+  void submitForm() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState?.save();
+
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      name: _formData['name'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+
+    print(newProduct.id);
+    print(newProduct.name);
+    print(newProduct.description);
+    print(newProduct.price);
+    print(newProduct.imageUrl);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,17 +72,40 @@ class _ProductFormPageState extends State<ProductFormPage> {
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
         iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            onPressed: submitForm,
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
-                decoration: const InputDecoration(labelText: "Nome"),
+                decoration: const InputDecoration(
+                  labelText: "Nome",
+                ),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocus);
+                },
+                onSaved: (name) => _formData["name"] = name ?? "",
+                validator: (nameForm) {
+                  final name = nameForm ?? '';
+
+                  if (name.trim().isEmpty) {
+                    return "Nome é obrigatório";
+                  }
+
+                  if (name.trim().length < 3) {
+                    return "Nome precisa no mínimo de 3 letras";
+                  }
+
+                  return null;
                 },
               ),
               TextFormField(
@@ -63,14 +116,37 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocus);
                 },
+                onSaved: (price) => _formData["price"] = double.parse(price ?? "0"),
+                validator: (price) {
+                  if (price != null && price.isEmpty) {
+                    return "É obrigatório informar um valor";
+                  }
+
+                  if (double.parse(price.toString()) < 0) {
+                    return "O valor precisa ser positivo";
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(labelText: "Descrição"),
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
-                onFieldSubmitted: (_) {
-                  FocusScope.of(context).requestFocus(_imageUrlFocus);
+                onSaved: (description) => _formData["description"] = description ?? "",
+                validator: (descriptionForm) {
+                  final description = descriptionForm ?? '';
+
+                  if (description.trim().isEmpty) {
+                    return "Descrição é obrigatório";
+                  }
+
+                  if (description.trim().length < 10) {
+                    return "Descrição precisa no mínimo de 10 letras";
+                  }
+
+                  return null;
                 },
               ),
               Row(
@@ -83,6 +159,8 @@ class _ProductFormPageState extends State<ProductFormPage> {
                       textInputAction: TextInputAction.done,
                       focusNode: _imageUrlFocus,
                       controller: _imageUrlController,
+                      onFieldSubmitted: (_) => submitForm(),
+                      onSaved: (imageUrl) => _formData["imageUrl"] = imageUrl ?? "",
                     ),
                   ),
                   Container(
