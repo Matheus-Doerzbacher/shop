62d93ee4f18898aca0140e6dfe81e12d2cb52c61
MathePLay <Matheus.doer@gmail.com>
2023-11-24 09:14:47 -0300

    finalizado o modulo de requisições http

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,32 +4,15 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_item.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
-  @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) {
-      setState(() => _isLoading = false);
-    });
+  Future<void> _refreshOrders(BuildContext context) {
+    return Provider.of<OrderList>(context, listen: false).loadOrders();
   }
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -40,19 +23,27 @@ class _OrdersPageState extends State<OrdersPage> {
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Padding(
-              padding: const EdgeInsets.all(10),
-              child: ListView.builder(
-                itemCount: orders.itemsCount,
-                itemBuilder: (ctx, i) {
-                  return OrderItem(order: orders.items[i]);
-                },
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => RefreshIndicator(
+                onRefresh: () => _refreshOrders(context),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (ctx, i) => OrderItem(order: orders.items[i]),
+                  ),
+                ),
               ),
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 }
