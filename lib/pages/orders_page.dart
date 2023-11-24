1e3f57555c74b70f85928caa54e92ebf4183d255
MathePLay <Matheus.doer@gmail.com>
2023-11-24 08:53:38 -0300

    obtendo os dados dos pedidos do FB

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,8 +4,27 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_item.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({super.key});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((_) {
+      setState(() => _isLoading = false);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,15 +40,19 @@ class OrdersPage extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       drawer: const AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView.builder(
-          itemCount: orders.itemsCount,
-          itemBuilder: (ctx, i) {
-            return OrderItem(order: orders.items[i]);
-          },
-        ),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(10),
+              child: ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) {
+                  return OrderItem(order: orders.items[i]);
+                },
+              ),
+            ),
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,4 +2,5 @@
 
 class Constants {
   static const PRODUCT_BASE_URL = "https://shop-flutter-a0886-default-rtdb.firebaseio.com/products";
+  static const ORDER_BASE_URL = "https://shop-flutter-a0886-default-rtdb.firebaseio.com/orders";
 }
