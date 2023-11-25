60f1e88f34f8c4daffbe00ddc7767f28453d0fd6
Matheus Doerzbacher <matheus.doer@gmail.com>
2023-11-25 11:09:36 -0300

    Ajustes na tela de cadastro

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -66,7 +66,7 @@ class _AuthFormState extends State<AuthForm> {
       child: Container(
         color: Colors.white,
         padding: const EdgeInsets.all(16),
-        height: _isLogin() ? 330 : 400,
+        height: _isLogin() ? 330 : 420,
         width: deviceSize.width * 0.75,
         child: Form(
           key: _formKey,
