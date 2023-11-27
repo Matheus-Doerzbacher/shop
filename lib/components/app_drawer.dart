571771828c3a299e1f301624ea04d132c67424ff
Matheus Doerzbacher <matheus.doer@gmail.com>
2023-11-27 15:40:56 -0300

    adicionada o metodo de logout

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/auth.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -8,40 +10,62 @@ class AppDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          AppBar(
-            title: const Text("Bem Vindo Usuário!"),
-            automaticallyImplyLeading: false,
+          Column(
+            children: [
+              AppBar(
+                title: const Text("Bem Vindo Usuário!"),
+                automaticallyImplyLeading: false,
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.shop),
+                title: const Text("Loja"),
+                onTap: () {
+                  Navigator.of(context).pushReplacementNamed(
+                    AppRoutes.AUTH_OR_HOME,
+                  );
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.payment),
+                title: const Text("Pedidos"),
+                onTap: () {
+                  Navigator.of(context).pushReplacementNamed(
+                    AppRoutes.ORDERS,
+                  );
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.edit),
+                title: const Text("Gerenciar Produtos"),
+                onTap: () {
+                  Navigator.of(context).pushReplacementNamed(
+                    AppRoutes.PRODUCTS,
+                  );
+                },
+              ),
+              const Divider(),
+            ],
           ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.shop),
-            title: const Text("Loja"),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed(
-                AppRoutes.AUTH_OR_HOME,
-              );
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.payment),
-            title: const Text("Pedidos"),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed(
-                AppRoutes.ORDERS,
-              );
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.edit),
-            title: const Text("Gerenciar Produtos"),
-            onTap: () {
-              Navigator.of(context).pushReplacementNamed(
-                AppRoutes.PRODUCTS,
-              );
-            },
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30),
+            child: ListTile(
+              leading: const Icon(Icons.exit_to_app, color: Colors.red),
+              title: const Text(
+                "Sair",
+                style: TextStyle(color: Colors.red),
+              ),
+              onTap: () {
+                Provider.of<Auth>(context, listen: false).logout();
+                Navigator.of(context).pushReplacementNamed(
+                  AppRoutes.AUTH_OR_HOME,
+                );
+              },
+            ),
           ),
         ],
       ),
